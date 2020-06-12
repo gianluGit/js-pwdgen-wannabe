6bee4c22baf94c341d14bcf05bd3f67ee4344609
Gianluca <gianlucarecchia1993@gmail.com>
2020-06-12 15:47:27 +0200

    esercizio var js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// chiedere all'utente il suo nome
+var nome = prompt("inserisci il tuo nome");
+
+// chiedere il suo cognome
+var cognome = prompt("inserisci il tuo cognome");
+
+// chiedere il suo colore preferito
+var colore = prompt("inserisci il tuo colore preferito");
+
+// fare apparire l'output nomeCognomeColorepreferito19
+document.getElementById('output-js').innerHTML = nome + cognome + colore + "19";
